c711f452bd8c730094e81b33341bde83bc176be7
Alena Dagneau <alenadagneau@gmail.com>
2016-01-07 18:04:14 +0100

    initial commit

---
+++ js/contact.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+    
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "http://formspree.io/alena@makeschool.com", 
+            method: "POST",
+            data: {
+                email: $('.email').val(),
+                message: $('.message').val()
+            },
+            dataType: "json"
+        }).error(function(x, status, error){
+            alert('Uh oh, something went wrong. Please try again.');
+        }). success(function(data, status) {
+            alert('Thank you for your message. Kitty will get back to you soon.');
+        });
+    });
+
+});
